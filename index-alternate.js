7196ea503df5f37eea2b4cce537a6c080a3f4fa3
AmitShashi <73923245+AmitShashi@users.noreply.github.com>
2023-12-31 00:43:05 +0530

    Create index-alternate.js

---
+++ index-alternate.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+app.use(express.json());
+
+const url = 'mongodb+srv://amit:amit@cluster0.6nkx33w.mongodb.net'; //replace url
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let db;
+client.connect().then(() => {
+  db = client.db('mynotebook'); //replace db name
+  console.log('Connected to MongoDB');
+});
+
+// CRUD Routes
+
+// Fetch all documents
+app.get('/api/data', async (req, res) => {
+  try {
+    const collection = db.collection('notes'); // replace collection/table name
+    const data = await collection.find({}).toArray();
+    res.json(data);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// Add more routes as needed...
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
